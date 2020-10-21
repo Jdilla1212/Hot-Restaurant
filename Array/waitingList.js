e60decc12ff16ae3cb3d3441cf522e15c93bd31a
Josh Padilla <jdp237@nau.edu>
2020-10-20 21:03:56 -0700

    added html and api routes

--- Array/waitingList.js
+++ Array/waitingList.js
@@ -1,6 +1,6 @@
 const waitingListArray = [
     {
-        name: "tableArray",
+        name: "name",
         Number: "999-999-9999", 
         email: "tableArray@example.com",
         ID: "hdfghdfhdfghdfgh"
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // use express
+const { table } = require("console");
 const express = require("express");
-const  path = require("path");
+const path = require("path");
+const tableArray = require("./Array/table.js");
+const tables = require("./Array/table.js");
+const waitList = require("./Array/waitingList.js");
+// console.log(tables, waitList);
 
 const app = express();
 
@@ -10,17 +15,42 @@ const PORT = 3000;
 
 
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 
 
-require("")(app);
-require("")(app);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/home.html"));
+});
+
+app.get("/reservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/reservations.html"));
+});
+
+app.get("/table", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/tables.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitingList", function (req, res) {
+    return res.json(waitList);
+});
 
 
+app.post("/api/tables", function (req, res) {
+    if (tables.length < 5) {
+        tables.push(req.body)
+    } else {
+        waitList.push(req.body)
+    }
+});
 
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("Welcome to port: " + PORT);
 })
